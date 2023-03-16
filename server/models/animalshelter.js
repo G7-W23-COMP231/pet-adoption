2a4e4156e20f566c3853d008f65ca3c6ca8e3a11
bhavitha111 <113801858+bhavitha111@users.noreply.github.com>
2023-03-16 14:17:35 -0400

    mongoschema

--- server/models/animalshelter.js
+++ server/models/animalshelter.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const animalSchema = new Schema({
   // Define the properties of your animalschema here
   userName: String,
+<<<<<<< Updated upstream
   password : String,
   animalShleterName: String,
   location: String,
@@ -12,3 +13,16 @@ const animalSchema = new Schema({
   petId: Number,
 });
 const User = mongoose.model('User', animalSchema);
+=======
+  password: String,
+  animalShelterName: String,
+  location: String,
+  phoneNumber: Number,
+},{
+  //Threepat- for update condition
+  collection: "animalshelter",
+  timestamps: true
+}
+);
+module.exports = mongoose.model('animalshelter', animalSchema);
+>>>>>>> Stashed changes
---
+++ server/router/animalshelter.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Animal = require('../models/animalshelter');
+
+const router = express.Router();
+
+router.get('/info', async (req, res) => {
+  try {
+    const animals = await Animal.find();
+    res.json(animals);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+router.post('/info', async (req, res) => {
+  const { userName, password, animalShelterName, location, phoneNumber } = req.body;
+
+  try {
+    const animal = new Animal({ 
+        userName, 
+        password, 
+        animalShelterName, 
+        location, 
+        phoneNumber, 
+         });
+    await animal.save();
+    res.json(animal);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
