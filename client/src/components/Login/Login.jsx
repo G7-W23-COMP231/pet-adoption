f9fc2b332846d2666062f028037961ef64c88694
Racyn Ibaya <racynibaya@outlook.com>
2023-03-17 11:41:40 -0400

    fixing the login ui overflow

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -4,6 +4,7 @@ import {
   HiOutlineLockClosed,
   HiLockClosed,
 } from 'react-icons/hi';
+import wave from '../../assets/wave.svg';
 import './Login.css';
 
 const defaultFormField = {
@@ -12,8 +13,10 @@ const defaultFormField = {
 };
 
 const Login = () => {
+  // State of Login Form
   const [formField, setFormField] = useState(defaultFormField);
 
+  // It will update the state for every input change
   const handleChange = event => {
     const { value, name } = event.target;
     setFormField({ ...formField, [name]: value });
@@ -21,44 +24,43 @@ const Login = () => {
 
   return (
     <div className='login__container'>
-      <div className='login__container-right'>
-        <div className='text__container'>
-          <h2>Welcome back!</h2>
-          <p>Open your heart and home to a furry friend - Adopt now!</p>
-        </div>
-        <form className='login__container-form'>
-          <div className='input__container'>
-            <HiOutlineMail className='email__icon' />
-            <input
-              className='login__input'
-              type='email'
-              name='email'
-              id='email'
-              placeholder='email'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='input__container'>
-            <HiLockClosed className='email__icon' />
-            <input
-              className='login__input'
-              type='password'
-              onChange={handleChange}
-              name='password'
-              id='password'
-              placeholder='password'
-            />
-          </div>
-          <div className='btn__container'>
-            <button type='button' className='login__btn btn_login btn-lg'>
-              Login
-            </button>
-            <button type='button' className='login__btn btn_signup btn-lg'>
-              Signup
-            </button>
-          </div>
-        </form>
+      <div className='text__container'>
+        <h2>Welcome back!</h2>
+        <p>Open your heart and home to a furry friend &mdash; Adopt now!</p>
       </div>
+      <form className='login__container-form'>
+        <div className='input__container'>
+          <HiOutlineMail className='email__icon' />
+          <input
+            className='login__input'
+            type='email'
+            name='email'
+            id='email'
+            placeholder='email'
+            onChange={handleChange}
+          />
+        </div>
+        <div className='input__container'>
+          <HiLockClosed className='email__icon' />
+          <input
+            className='login__input'
+            type='password'
+            onChange={handleChange}
+            name='password'
+            id='password'
+            placeholder='password'
+          />
+        </div>
+        <div className='btn__container'>
+          <button type='button' className='login__btn btn_login btn-lg'>
+            Login
+          </button>
+          <button type='button' className='login__btn btn_signup btn-lg'>
+            Signup
+          </button>
+        </div>
+      </form>
+      <img className='wave' src={wave} alt='wave' />
     </div>
   );
 };
