4bf5de2f2b3f7e16f4fe5088d1c19396e3f8e58b
bhavitha111 <113801858+bhavitha111@users.noreply.github.com>
2023-03-19 22:19:09 -0400

    petmodel

---
+++ server/models/pet.js
@@ -0,0 +1,35 @@
+//import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const petSchema = new Schema({
+  // Define the properties of your petschema here
+  petName : String,
+  petCategory : Number,
+  shelterID : String,
+  age : Number,
+  gwithFirstPetOwner : Number,
+  canGetAlongWithOtherPets : Number,
+  canGetWithHumanAge : String,
+  indoorOutdoor : Number,
+  petOusideHours : Number,
+  petWithOwnerAllTime : Number,
+  petStay : Number,
+  serviceAnimal : Number,
+  enthusiasticPet : Number,
+  playfulPet : Number,
+  laidbackPet : Number,
+  trainablePet : Number,
+  specialNeeds : Number,
+  petPhoto : Buffer,
+  medHistory : String,
+  behaveIssue : String,
+  vaccRecord : String,
+
+},{
+  collection: "pet",
+  timestamps: true
+}
+);
+module.exports = mongoose.model('pet', petSchema);
+
