811e581a48af877ee9da1a943237de47202c6d54
CKatya <112958124+CKatya@users.noreply.github.com>
2023-03-24 22:38:25 -0400

    Create pet components
    Create pet compomemts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,39 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+<<<<<<< Updated upstream
 
 function App() {
+=======
+import ShowPets from './components/ShelterPages/ShowPets';
+import AddPet from './components/ShelterPages/AddPet';
+import Navbar from './components/Navbar';
+import Pet from './components/Pet';
+
+function App() {
+  {
+    /* dummy routing to test authentication */
+  }
+  return (
+    <>
+      <nav>
+        <Navbar />
+      </nav>
+
+      <Routes>
+        <Route exact path='/' element={<h1>Homepage</h1>} />
+        <Route exact path='/signin' element={<Login />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/showpets' element={<ShowPets />} />
+        <Route exact path='/addpet' element={<AddPet />} />
+        <Route exact path='/pets' element={<Pet />} />
+      </Routes>
+    </>
+  );
+
+  {
+    /*
+>>>>>>> Stashed changes
   return (
     <div>
       <Register />
