01f3868228db880160cff98ad0493fee5d0cf291
CKatya <112958124+CKatya@users.noreply.github.com>
2023-03-27 20:39:05 -0400

    created user login and user register
    created user login and user register

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import './index.css';
-
+import "bootstrap/dist/css/bootstrap.min.css"; 
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import App from './App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
