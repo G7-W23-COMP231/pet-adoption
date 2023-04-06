dba0151185c0802d625051f0485c990118288201
Racyn Ibaya <racynibaya@outlook.com>
2023-04-06 16:03:49 -0400

    Log-in indication

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import LoginContainer from "./LoginContainer";
-import LoginForm from "./LoginForm";
+import LoginContainer from './LoginContainer';
+import LoginForm from './LoginForm';
 
 const defaultFormField = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 // Shelter LOGIN
@@ -16,31 +16,31 @@ const Login = () => {
   const [user, setUser] = useState(null);
 
   // It will update the state for every input change
-  const handleChange = (event) => {
+  const handleChange = event => {
     const { value, name } = event.target;
     setFormField({ ...formField, [name]: value });
   };
 
-  const handleLogin = (event) => {
+  const handleLogin = event => {
     event.preventDefault();
 
     //fetch('http://localhost:5000/auth/login', {
-    fetch("http://localhost:5000/animalshelter/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    fetch('http://localhost:5000/animalshelter/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formField),
     })
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         if (data.token) {
-          localStorage.setItem("token", data.token);
+          localStorage.setItem('token', data.token);
           //getUserInfo();
-          navigate("/showpets");
+          navigate('/showpets');
         } else {
           alert(data.message);
         }
       })
-      .catch((err) => alert("Access Denied"));
+      .catch(err => alert('Access Denied'));
   };
 
   // const getUserInfo = () => {
@@ -61,11 +61,11 @@ const Login = () => {
   // };
 
   return (
-    <LoginContainer>
+    <LoginContainer type='shelter'>
       <LoginForm
         handleChange={handleChange}
         handleLogin={handleLogin}
-        type={"shelter"}
+        type={'shelter'}
         user={user}
       />
     </LoginContainer>
